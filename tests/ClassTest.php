3e5ef5a93ff27fae223687d55f72eb24c1c027e4
Markus Hackspacher <hackspacher@gmx.de>
2017-03-23 10:31:50 +0100

    Create ClassTest.php

---
+++ tests/ClassTest.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'core/functions.php';
+require_once 'core/application.php';
+
+class ClassTest extends PHPUnit\Framework\TestCase
+{
+    public function testClassRouter()
+    {
+        $test = Router();
+    }
+}
+?>
