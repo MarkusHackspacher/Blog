958b5cebee84d2db48ed3640b0686ea22a471286
Markus Hackspacher <hackspacher@gmx.de>
2017-06-01 08:57:05 +0200

    Update TestApplication.php

--- tests/TestApplication.php
+++ tests/TestApplication.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'core/application.php';
 
-class BlogTest extends PHPUnit\Framework\TestCase
+class TestApplication extends PHPUnit\Framework\TestCase
 # Example from https://github.com/travis-ci-examples/php
 {
     public function testset_exception_handler()
