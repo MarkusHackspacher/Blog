52e09329b61ea6e3d6edd9ff82e86676f0a21557
Markus Hackspacher <hackspacher@gmx.de>
2017-06-01 08:53:22 +0200

    Update TestApplication.php

--- tests/TestApplication.php
+++ tests/TestApplication.php
@@ -6,6 +6,7 @@ class BlogTest extends PHPUnit\Framework\TestCase
 {
     public function testset_exception_handler()
     {
-         $test = set_exception_handler();
+        $test = set_exception_handler();
+        $this->assertEquals(1, $test);
     }
 }
