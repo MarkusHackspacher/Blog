e393f1838f79fd0990b5f1c9a59577000ecde8c2
Markus Hackspacher <hackspacher@gmx.de>
2022-08-03 17:10:15 +0200

    add void

--- tests/BlogTest.php
+++ tests/BlogTest.php
@@ -9,14 +9,14 @@ class BlogTest extends PHPUnit\Framework\TestCase
      */
     private $pdo;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
     }
 
-    public function tearDown()
+    public function tearDown(): void
     {
         $this->pdo->query("DROP TABLE hello");
     }
